6b20c806b38bf85dba8b024cad6e44d43a6ddcc8
Luis Reyes <iscluisreyes@gmail.com>
2022-10-06 17:36:44 -0500

    Luis Reyes
    Configurando Metaetiquetas y SEO , cambiamos favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,8 +3,8 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - AryyMedico',
-    title: 'AryyMedico',
+    titleTemplate: '%s - Aryy',
+    title: 'Aryy',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -58,7 +58,7 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      dark: false,
       themes: {
         dark: {
           primary: colors.blue.darken2,
--- pages/paciente.vue
+++ pages/paciente.vue
@@ -5,24 +5,27 @@
 </template>
 
 <script>
-    export default {
-        head(){
-            return{
-                title: 'Pacientes | Aryy',
-                meta: [
-                    {
-                       hid:"description",
-                       name: "description",
-                       content: "mi lista de pacientes en Aryy" 
-                    }
-                ],
-                htmlAttrs: {
-                    lang: "en"
-                }
+/**Nuxt te permite definir todos los predeterminados <meta> etiquetas 
+ * para su aplicación dentro del archivo nuxt.config.js usando la propiedad head. 
+ * Esto es muy útil para agregar un título predeterminado y una etiqueta de descripción 
+ * con fines de SEO o para configurar la ventana gráfica o agregar el favicon.  */
 
-
+export default {
+    head: {
+        title: 'Pacientes',
+        /*creamos un arreglos con objertos */
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            {
+                hid: 'description',
+                name: 'description',
+                content: 'lista de pacientes de Aryy'
             }
-        }
+        ],
+       /*  link: [{ rel: 'icon', type: 'image/x-icon', href: '@/static/favicon.ico' }] */
     }
+    /*Esto le dará el mismo título y descripción en cada página.  */
+}
 
 </script>
