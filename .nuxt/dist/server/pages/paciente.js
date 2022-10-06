7980a265e6dc9509c32f0e950d0e93d94555a24b
Luis Reyes <iscluisreyes@gmail.com>
2022-10-06 17:46:10 -0500

    Luis Reyes
    nuxt generate

---
+++ .nuxt/dist/server/pages/paciente.js
@@ -0,0 +1,78 @@
+exports.ids = [8];
+exports.modules = {
+
+/***/ 147:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// CONCATENATED MODULE: ./node_modules/vuetify-loader/lib/loader.js??ref--4!./node_modules/babel-loader/lib??ref--2-0!./node_modules/vue-loader/lib/loaders/templateLoader.js??ref--7!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./pages/paciente.vue?vue&type=template&id=882a970e&
+var render = function render() {
+  var _vm = this,
+      _c = _vm._self._c;
+
+  return _c('div', [_vm._ssrNode("<h1>Paciente</h1>")]);
+};
+
+var staticRenderFns = [];
+
+// CONCATENATED MODULE: ./pages/paciente.vue?vue&type=template&id=882a970e&
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib??ref--2-0!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./pages/paciente.vue?vue&type=script&lang=js&
+/**Nuxt te permite definir todos los predeterminados <meta> etiquetas 
+ * para su aplicación dentro del archivo nuxt.config.js usando la propiedad head. 
+ * Esto es muy útil para agregar un título predeterminado y una etiqueta de descripción 
+ * con fines de SEO o para configurar la ventana gráfica o agregar el favicon.  */
+/* harmony default export */ var pacientevue_type_script_lang_js_ = ({
+  head: {
+    title: 'Pacientes',
+
+    /*creamos un arreglos con objertos */
+    meta: [{
+      charset: 'utf-8'
+    }, {
+      name: 'viewport',
+      content: 'width=device-width, initial-scale=1'
+    }, {
+      hid: 'description',
+      name: 'description',
+      content: 'lista de pacientes de Aryy'
+    }]
+    /*  link: [{ rel: 'icon', type: 'image/x-icon', href: '@/static/favicon.ico' }] */
+
+  }
+  /*Esto le dará el mismo título y descripción en cada página.  */
+
+});
+// CONCATENATED MODULE: ./pages/paciente.vue?vue&type=script&lang=js&
+ /* harmony default export */ var pages_pacientevue_type_script_lang_js_ = (pacientevue_type_script_lang_js_); 
+// EXTERNAL MODULE: ./node_modules/vue-loader/lib/runtime/componentNormalizer.js
+var componentNormalizer = __webpack_require__(14);
+
+// CONCATENATED MODULE: ./pages/paciente.vue
+
+
+
+
+
+/* normalize component */
+
+var component = Object(componentNormalizer["a" /* default */])(
+  pages_pacientevue_type_script_lang_js_,
+  render,
+  staticRenderFns,
+  false,
+  null,
+  null,
+  "55464400"
+  
+)
+
+/* harmony default export */ var paciente = __webpack_exports__["default"] = (component.exports);
+
+/***/ })
+
+};;
+//# sourceMappingURL=paciente.js.map
